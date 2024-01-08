0c7420cd34227ac8a858bb94c9bc824ff8f99f5d
DantesHell47 <Jefferson.dante47@gmail.com>
2024-01-08 00:06:57 -0300

    O código foi refatorado. Foi usado função haskey para vericar se a chave n existe no diocionario

--- fib_memo.jl
+++ fib_memo.jl
@@ -5,12 +5,11 @@
 dict_memo = Dict(0=>0, 1=> 1) # Dicionario vazio para armazenar os valores já calculado.
 
 function fib_recursivo_memo(n)
-    
-    if n in keys(dict_memo)
+
+    if haskey(dict_memo, n)
         return dict_memo[n]
     else
         dict_memo[n] = fib_recursivo_memo(n-1) + fib_recursivo_memo(n-2)
     end
     return dict_memo[n]
-
 end
