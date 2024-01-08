e42167332ab1dfaf6b31a242f60e3e0bb7d0ea63
DantesHell47 <Jefferson.dante47@gmail.com>
2024-01-07 22:12:09 -0300

    Foi feita a versão iterativa e a versao recursiva para calcular a sequencia de Fibonacci

--- seq_fib_iterativa.jl
+++ seq_fib_iterativa.jl
@@ -1,11 +1,22 @@
-function fib(n)
-    if n == 0 || n == 1
+# Exercicio para contruir a sequencia de Fibonacci para o caso iterativo e para o caso recursivo
+
+function fib_iterativo(n)
+    if n == 0 || n == 1 # Valores iniciais. Retorna 0 ou 1
         return n
     end
     last = 0
     next = 1
     for _ in 1:n
-        last, next = next, next + last
+        last, next = next, next + last # Desconpacatção. O `last` recebe o `next` e o `next` recebe a soma `last` + `next`
     end
-    return next
+    return next # Retorna o n-ésima posição da sequencia de Fibonacci
+end
+
+
+function fib_recursivo(n)
+    if n == 0 || n == 1
+        return n
+    else
+        return fib_recursivo(n-1) + fib_recursivo(n-2)
+    end
 end
