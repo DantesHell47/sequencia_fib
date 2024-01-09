c525cf9588708eb9f56ba31609b0e629d35e6ded
DantesHell47 <Jefferson.dante47@gmail.com>
2024-01-08 23:54:50 -0300

    Corrigi o código interativo da sequencia de Fibonacci e alterei o caso base da sequencia de Fibonacci recursiva

--- seq_fib_iterativa.jl
+++ seq_fib_iterativa.jl
@@ -4,10 +4,9 @@ function fib_iterativo(n)
     if n == 0 || n == 1 # Valores iniciais. Retorna 0 ou 1
         return n
     end
-    last = 0
-    next = 1
+    last, next = 0, 1
     for _ in 1:n
-        last, next = next, next + last # Desconpacatção. O `last` recebe o `next` e o `next` recebe a soma `last` + `next`
+        last, next = next, next + last # Descompacatção. O `last` recebe o `next` e o `next` recebe a soma `last` + `next`
     end
     return next # Retorna o n-ésima posição da sequencia de Fibonacci
 end
