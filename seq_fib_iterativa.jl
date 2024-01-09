0bbd2825c97e307ca89db174c55585a4c110e3aa
DantesHell47 <Jefferson.dante47@gmail.com>
2024-01-09 00:55:10 -0300

    Foi corrigido um erro de iteração.

--- seq_fib_iterativa.jl
+++ seq_fib_iterativa.jl
@@ -1,11 +1,11 @@
 # Exercicio para contruir a sequencia de Fibonacci para o caso iterativo e para o caso recursivo
 
 function fib_iterativo(n)
-    if n == 0 || n == 1 # Valores iniciais. Retorna 0 ou 1
+    if n == 0
         return n
     end
-    last, next = 0, 1
-    for _ in 1:n
+    last, next  = 0, 1
+    for i in 1:n-1
         last, next = next, next + last # Descompacatção. O `last` recebe o `next` e o `next` recebe a soma `last` + `next`
     end
     return next # Retorna o n-ésima posição da sequencia de Fibonacci
@@ -13,7 +13,7 @@ end
 
 
 function fib_recursivo(n)
-    if n == 0 || n == 1
+    if n < 2
         return n
     else
         return fib_recursivo(n-1) + fib_recursivo(n-2)
