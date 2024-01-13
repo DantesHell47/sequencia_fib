262a8b3eeabc1d19389efd666eb91ae4b541ee0c
DantesHell47 <Jefferson.dante47@gmail.com>
2024-01-12 23:16:51 -0300

    Refatorei o código. Foi adicionado uma segunda versao da function fib_recursivo_memo

--- fib_memo.jl
+++ fib_memo.jl
@@ -14,3 +14,17 @@ function fib_recursivo_memo(n, dict_memo = Dict{Int,Int}(0=>0, 1=>1))
     end
     return dict_memo[n]
 end
+
+
+# Uma outra maneira de fazer este código acima é a seguinte:
+
+function fib_recursivo_memo1(n, dict_memo = Dict{Int,Int}(0=>0, 1=>1))
+    if n < 0
+        error("n não deve ser um número negativo")
+    end
+    
+    if !haskey(dict_memo, n)
+        dict_memo[n] = fib_recursivo_memo(n-1, dict_memo) + fib_recursivo_memo(n-2, dict_memo)
+    end
+    return dict_memo[n]
+end
